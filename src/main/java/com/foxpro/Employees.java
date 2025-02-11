65f64b7b4086241818110fe10f54383838947853
Kushal Khandal <kushal2001khandal@gmail.com>
2025-02-11 22:30:46 +0530

    added incentive reading for client csv file

--- src/main/java/com/foxpro/Employees.java
+++ src/main/java/com/foxpro/Employees.java
@@ -10,7 +10,6 @@ import com.foxpro.databaseManager.EmployeeDatabaseHandler;
 
 class Employees {
 
-
     private static final Logger logger = Logs.logger;
 
 
@@ -18,8 +17,6 @@ class Employees {
      * connection for employees
      * established inside constructor and closed inside the finally block inside function addEmployees
      */
-
-
     String month,
             region_optional,
             path_pf_csv,
@@ -66,18 +63,14 @@ class Employees {
         percentMsl2 = salaryStructure.msl2;
         percentMsl3 = salaryStructure.msl3;
 
-
         // if dir/db exists , else create dir/db
         Manager.checkAndCreateDir(pfRegNumber, year, month, region_optional);
 
         // now dir & db exists , initiate connection
         Manager.initiateEmployeesConnection(pfRegNumber, year, month, region_optional);
 
-
-
         // create table data
         Manager.executeCreateTableCommand(pfRegNumber, year, month, region_optional);
-
 
         // fill table with provided data
         Manager.executeFillTableCommand(pfRegNumber, year, month, region_optional, path_pf_csv);
@@ -89,7 +82,6 @@ class Employees {
         long uan = 0;
         String[] inputArr;
         String input;
-
 
         boolean incentiveProvided = false;
 
@@ -108,8 +100,6 @@ class Employees {
                 calc_incentive = 0,
                 pf_salary, pf_deduction, esic_salary, esic_deduction, total_deduction, calc_salary, netPayableAmount,
                 pf_paid_by_employee = 0, pf_paid_by_employer = 0;
-
-
 
         if (path_client_csv == null) {
             System.out.println("\n  enter exit / quit in uan to exit");
@@ -135,72 +125,72 @@ class Employees {
                 System.out.print("attendance : ");
                 attendance = Double.parseDouble(mainBufferedReader.readLine().trim());
 
-
                 System.out.print("include incentive : ");
-                if ( mainBufferedReader.readLine().trim().equalsIgnoreCase("y")){
-                    if ( incentiveProvided == false){
-                        System.out.print("add incentive amount : ");
-                        calc_incentive = Double.parseDouble(mainBufferedReader.readLine().trim());
-                        incentiveProvided = true;
-                    }
-                    else{
-                        System.out.println("incentive included and set to : " + calc_incentive);
-                    }
-                }
-                else{
+                if (mainBufferedReader.readLine().trim().equalsIgnoreCase("y")) {
+                    /*
+                    for setting incentive use case only for one time
+                     */
+                    // if ( incentiveProvided == false){
+                    //     System.out.print("add incentive amount : ");
+                    //     calc_incentive = Double.parseDouble(mainBufferedReader.readLine().trim());
+                    //     incentiveProvided = true;
+                    // }
+                    // else{
+                    //     System.out.println("incentive included and set to : " + calc_incentive);
+                    // }
+
+                    /*
+                    for taking incentive input everytime from the user for each entry
+                     */
+                    calc_incentive = Double.parseDouble(mainBufferedReader.readLine().trim());
+                } else {
                     calc_incentive = 0;
                 }
 
-                logger.info("salary structure details : ");
-                logger.log(Level.INFO, "basic % ; {0}", percentBasic);
-                logger.log(Level.INFO, "hra % : {0}", percentHra);
-                logger.log(Level.INFO, "conv % : {0}", percentConv);
-                logger.log(Level.INFO, "washing allowance % : {0}", percentWashingAllowance);
-                logger.log(Level.INFO, "overtime % : {0}", percentOvertime);
+                // logger.info("salary structure details : ");
+                // logger.log(Level.INFO, "basic % ; {0}", percentBasic);
+                // logger.log(Level.INFO, "hra % : {0}", percentHra);
+                // logger.log(Level.INFO, "conv % : {0}", percentConv);
+                // logger.log(Level.INFO, "washing allowance % : {0}", percentWashingAllowance);
+                // logger.log(Level.INFO, "overtime % : {0}", percentOvertime);
+                total_basic = Math.ceil((total_salary * percentBasic) / 100);
+                total_hra = Math.ceil((total_salary * percentHra) / 100);
+                total_conv = Math.ceil((total_salary * percentConv) / 100);
+                total_washingAllowance = Math.ceil((total_salary * percentWashingAllowance) / 100);
+                total_overtime = Math.ceil((total_salary * percentOvertime) / 100);
 
-                total_basic = Math.ceil( (total_salary * percentBasic) / 100 );
-                total_hra = Math.ceil( (total_salary * percentHra) / 100 );
-                total_conv =Math.ceil ( (total_salary * percentConv) / 100 );
-                total_washingAllowance = Math.ceil( (total_salary * percentWashingAllowance) / 100 );
-                total_overtime = Math.ceil( (total_salary * percentOvertime) / 100 );
+                // logger.log(Level.INFO , "total basic : {0}" , total_basic);
+                // logger.log(Level.INFO , "total hra : {0}" , total_hra);
+                // logger.log(Level.INFO , "total conv : {0}" , total_conv);
+                // logger.log(Level.INFO , "total washingAllowance : {0}" , total_washingAllowance);
+                // logger.log(Level.INFO , "total overtime : {0}" , total_overtime);
+                calc_basic = Math.ceil((total_basic * attendance) / daysInMonth);
+                calc_hra = Math.ceil((total_hra * attendance) / daysInMonth);
+                calc_conv = Math.ceil((total_conv * attendance) / daysInMonth);
+                calc_overtime = Math.ceil((total_overtime * attendance) / daysInMonth);
+                calc_washingAllowance = Math.ceil((total_washingAllowance * attendance) / daysInMonth);
 
-                logger.log(Level.INFO , "total basic : {0}" , total_basic);
-                logger.log(Level.INFO , "total hra : {0}" , total_hra);
-                logger.log(Level.INFO , "total conv : {0}" , total_conv);
-                logger.log(Level.INFO , "total washingAllowance : {0}" , total_washingAllowance);
-                logger.log(Level.INFO , "total overtime : {0}" , total_overtime);
-
-                calc_basic = Math.ceil( (total_basic * attendance) / daysInMonth );
-                calc_hra = Math.ceil( (total_hra * attendance) / daysInMonth );
-                calc_conv = Math.ceil( (total_conv * attendance) / daysInMonth );
-                calc_overtime = Math.ceil( (total_overtime * attendance) / daysInMonth );
-                calc_washingAllowance = Math.ceil( (total_washingAllowance * attendance) / daysInMonth );
-
-                logger.log(Level.INFO , "calc basic : {0}" , calc_basic);
-                logger.log(Level.INFO , "calc hra : {0}" , calc_hra);
-                logger.log(Level.INFO , "calc conv : {0}" , calc_conv);
-                logger.log(Level.INFO , "calc washingAllowance : {0}" , calc_washingAllowance);
-                logger.log(Level.INFO , "calc overtime : {0}" , calc_overtime);
-
+                // logger.log(Level.INFO , "calc basic : {0}" , calc_basic);
+                // logger.log(Level.INFO , "calc hra : {0}" , calc_hra);
+                // logger.log(Level.INFO , "calc conv : {0}" , calc_conv);
+                // logger.log(Level.INFO , "calc washingAllowance : {0}" , calc_washingAllowance);
+                // logger.log(Level.INFO , "calc overtime : {0}" , calc_overtime);
                 pf_salary = calc_basic;
-                esic_salary = Math.ceil( (calc_basic + calc_hra + calc_conv + calc_overtime) );
-                calc_salary = Math.ceil( (esic_salary + calc_washingAllowance + calc_incentive) );
+                esic_salary = Math.ceil((calc_basic + calc_hra + calc_conv + calc_overtime));
+                calc_salary = Math.ceil((esic_salary + calc_washingAllowance + calc_incentive));
 
-                logger.log(Level.INFO , "esic_salary : {0}" , esic_salary);
-                logger.log(Level.INFO , "calc_salary : {0}" , calc_salary);
+                // logger.log(Level.INFO , "esic_salary : {0}" , esic_salary);
+                // logger.log(Level.INFO , "calc_salary : {0}" , calc_salary);
+                pf_deduction = Math.ceil((pf_salary * PF_DEDUCTION_RATE) / 100);
+                esic_deduction = Math.ceil((esic_salary * ESIC_DEDUCTION_RATE) / 100);
 
-                pf_deduction = Math.ceil( (pf_salary * PF_DEDUCTION_RATE) / 100 );
-                esic_deduction = Math.ceil( (esic_salary * ESIC_DEDUCTION_RATE) / 100 );
+                // logger.log(Level.INFO , "pf Deduction : {0}" , pf_deduction);
+                // logger.log(Level.INFO , "esic Deduction : {0}" , esic_deduction);
+                total_deduction = Math.ceil((pf_deduction + esic_deduction));
+                netPayableAmount = Math.ceil(calc_salary - total_deduction);
 
-                logger.log(Level.INFO , "pf Deduction : {0}" , pf_deduction);
-                logger.log(Level.INFO , "esic Deduction : {0}" , esic_deduction);
-
-                total_deduction = Math.ceil( (pf_deduction + esic_deduction) );
-                netPayableAmount =Math.ceil (calc_salary - total_deduction);
-
-                logger.log(Level.INFO , "total deduction : {0}" , total_deduction);
-                logger.log(Level.INFO , "Net Payable Amount : {0}" , netPayableAmount);
-
+                // logger.log(Level.INFO , "total deduction : {0}" , total_deduction);
+                // logger.log(Level.INFO , "Net Payable Amount : {0}" , netPayableAmount);
                 EmployeeDatabaseHandler.insertEmployeeDetails(
                         new double[]{
                             total_salary,
@@ -236,7 +226,6 @@ class Employees {
 
 
             /* add incentive  */
-
             try {
                 fileReader = new FileReader(path_client_csv);
                 BufferedReader fileBufferedReader = new BufferedReader(fileReader);
@@ -247,20 +236,39 @@ class Employees {
 
                     uan = Long.parseLong(inputArr[0]);
 
-                    if (inputArr.length == 4 || inputArr.length == 3) {
-                        // first 2 columns : uan_no and name
+                    // add one column for incentive also
+                    // if (inputArr.length == 5 || inputArr.length == 4 || inputArr.length == 3) {
+                    if (inputArr.length == 5 || inputArr.length == 4 ) {
                         // EmployeeDatabaseHandler.printEmployeeDetails(inputArr[0]);
+                        /*
+                         * first 2 columns if length == 4 then uan and name : then salary and attendance
+                         * else only first column uan : then salary and attendance
+                         * only name is optional , else uan , total_salary , attendance , calc_incentive compulsory
+                         */
 
-                        if (inputArr.length == 3) {
-                            total_salary = Double.parseDouble(inputArr[1]);
-                            attendance = Double.parseDouble(inputArr[2]);
-                        } else {
+                        if (inputArr.length == 5) {
+                            // uan , name , salary , attendance , incentive
                             total_salary = Double.parseDouble(inputArr[2]);
                             attendance = Double.parseDouble(inputArr[3]);
+                            calc_incentive = Double.parseDouble(inputArr[4]);
+
+                        } else {
+                            // uan , salary , attendance , incentive
+                            total_salary = Double.parseDouble(inputArr[1]);
+                            attendance = Double.parseDouble(inputArr[2]);
+                            calc_incentive = Double.parseDouble(inputArr[3]);
                         }
+                        // else {
+                        //     // uan , name , salary , attendance : 4 cols
+                        //     total_salary = Double.parseDouble(inputArr[2]);
+                        //     attendance = Double.parseDouble(inputArr[3]);
+                        // }
 
                     } else if (inputArr.length == 2) {
-                        // cols : uan_no , salary
+                        /*
+                         * cols : uan_no , salary  : included into the client csv file
+                         * taking input for attendance
+                         */
                         EmployeeDatabaseHandler.printEmployeeDetails(inputArr[0]);
 
                         total_salary = Double.parseDouble(inputArr[1]);
@@ -269,6 +277,10 @@ class Employees {
                         attendance = Double.parseDouble(mainBufferedReader.readLine().trim());
 
                     } else if (inputArr.length == 1) {
+                        /*
+                         * cols : uan only
+                         * taking input for salary and attendance
+                         */
                         System.out.print("total salary : ");
                         total_salary = Double.parseDouble(mainBufferedReader.readLine().trim());
 
@@ -279,28 +291,27 @@ class Employees {
                         System.out.println("empty client csv file");
                         break;
                     }
-                    total_basic = Math.ceil( (total_salary * percentBasic) / 100 );
-                    total_hra =Math.ceil (total_salary * percentHra) / 100;
-                    total_conv =Math.ceil (total_salary * percentConv) / 100;
-                    total_washingAllowance = Math.ceil( (total_salary * percentWashingAllowance) / 100 );
-                    total_overtime = Math.ceil( (total_salary * percentOvertime) / 100 );
+                    total_basic = Math.ceil((total_salary * percentBasic) / 100);
+                    total_hra = Math.ceil(total_salary * percentHra) / 100;
+                    total_conv = Math.ceil(total_salary * percentConv) / 100;
+                    total_washingAllowance = Math.ceil((total_salary * percentWashingAllowance) / 100);
+                    total_overtime = Math.ceil((total_salary * percentOvertime) / 100);
 
-                    calc_basic = Math.ceil( (total_basic * attendance) / daysInMonth );
-                    calc_hra = Math.ceil( (total_hra * attendance) / daysInMonth );
-                    calc_conv = Math.ceil( (total_conv * attendance) / daysInMonth );
-                    calc_overtime = Math.ceil( (total_overtime * attendance) / daysInMonth );
-                    calc_washingAllowance = Math.ceil( (total_washingAllowance * attendance) / daysInMonth );
+                    calc_basic = Math.ceil((total_basic * attendance) / daysInMonth);
+                    calc_hra = Math.ceil((total_hra * attendance) / daysInMonth);
+                    calc_conv = Math.ceil((total_conv * attendance) / daysInMonth);
+                    calc_overtime = Math.ceil((total_overtime * attendance) / daysInMonth);
+                    calc_washingAllowance = Math.ceil((total_washingAllowance * attendance) / daysInMonth);
 
                     pf_salary = calc_basic;
-                    esic_salary = Math.ceil( (calc_basic + calc_hra + calc_conv + calc_overtime) );
-                    calc_salary = Math.ceil( (esic_salary + calc_washingAllowance + calc_incentive) );
+                    esic_salary = Math.ceil((calc_basic + calc_hra + calc_conv + calc_overtime));
+                    calc_salary = Math.ceil((esic_salary + calc_washingAllowance + calc_incentive));
 
-                    pf_deduction = Math.ceil( (pf_salary * PF_DEDUCTION_RATE) / 100 );
-                    esic_deduction = Math.ceil( (esic_salary * ESIC_DEDUCTION_RATE) / 100 );
+                    pf_deduction = Math.ceil((pf_salary * PF_DEDUCTION_RATE) / 100);
+                    esic_deduction = Math.ceil((esic_salary * ESIC_DEDUCTION_RATE) / 100);
 
-                    total_deduction = Math.ceil( (pf_deduction + esic_deduction) );
-                    netPayableAmount = Math.ceil( (calc_salary - total_deduction) );
-
+                    total_deduction = Math.ceil((pf_deduction + esic_deduction));
+                    netPayableAmount = Math.ceil((calc_salary - total_deduction));
 
                     EmployeeDatabaseHandler.insertEmployeeDetails(
                             new double[]{
